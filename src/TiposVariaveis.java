f4063d7d8849268b14e7fd14079fe486cb355f8b
amanjonson22 <amandacsr10@gmail.com>
2023-08-30 16:37:36 -0300

    adição de codigos

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+        /*Final faz com que a variável vire constante e precisa ser escrita em caixa alta. */
+
+        String meuNome = "Amanda Caroline";
     }
 }
